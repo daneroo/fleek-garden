3acb8b97e7ee46233b2ad2f17ebda106f8b95953
daniel <daniel.lauzon@gmail.com>
2023-08-04 22:53:47 +0000

    fleek: apply

--- user.nix
+++ user.nix
@@ -1,5 +1,16 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
   programs.starship.settings = {
+    format = lib.concatStrings [
+      "$username"
+      "$hostname"
+      "$kubernetes"
+      "$directory"
+      "$git_branch"
+      "$git_state"
+      "$git_status"
+      "$cmd_duration"
+      "$character"
+    ];
   };
 }
