e421d2b430fd9389f291692b8b83b49d725119b5
Daniel Lauzon <daniel.lauzon@gmail.com>
2023-08-08 19:31:13 +0000

    fleek: update flake.lock

--- ubunixarm/custom.nix
+++ ubunixarm/custom.nix
@@ -1,7 +1,7 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
   programs.bash.initExtra = ''
-    # My Own hist specific (ubunixarm) override of the .bashrc
+    # My Own host specific (ubunixarm) override of the .bashrc
   '';
 
  
