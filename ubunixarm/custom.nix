8305245b415bcb6bf8634eaf0b2e05addbd071ec
Daniel Lauzon <daniel.lauzon@gmail.com>
2023-08-08 19:17:51 +0000

    fleek: update flake.lock

--- ubunixarm/custom.nix
+++ ubunixarm/custom.nix
@@ -1,5 +1,12 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
+  programs.bash.initExtra = ''
+    My Own override of the .bashrc
+    if [ -f /etc/bashrc ]; then
+        . /etc/bashrc
+    fi
+    source <(fleek completion bash)
+  '';
 
  
 }
